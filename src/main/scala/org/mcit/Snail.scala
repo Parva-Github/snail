cc43bb5c6ac1f19bdb328563b1bdc6c6ff11a3e0
kishore09134 <63380692+kishore09134@users.noreply.github.com>
2020-05-27 19:09:00 -0400

    Update Snail.scala

--- src/main/scala/org/mcit/Snail.scala
+++ src/main/scala/org/mcit/Snail.scala
@@ -3,7 +3,7 @@ package org.mcit
 object array extends App{
 
   var list = List(List(1,2,3), List (4,5,6), List(7,8,9))
-val a = list.slice(0,3)
+val a = list.slice(0,3).reverse
   val b = list.slice(5,6)
   val c = list.slice(6,9).reverse
   val d = list.slice(3,5)
