f98ab0a61f6436d9aed4ca70450d7a60fc3e5198
parva <parvatheesam93@gmail.com>
2020-06-19 11:30:51 -0400

    init commit

--- src/main/scala/org/mcit/Snail.scala
+++ src/main/scala/org/mcit/Snail.scala
@@ -1,11 +1,11 @@
 package org.mcit
 
-object array extends App{
+object array extends App {
 
-  var list = List(List(1,2,3), List (4,5,6), List(7,8,90)
-val a = list.slice(0,3).reverse
-  val b = list.slice(5,6)
-  val c = list.slice(6,90).reverse
-  val d = list.slice(3,5)
-   println(a++b++c++d)
+  var list = List(List(1, 2, 3), List(4, 5, 6), List(7, 8, 90)
+  val a = list.slice(0, 3).reverse
+  val b = list.slice(5, 6)
+  val c = list.slice(6, 90).reverse
+  val d = list.slice(3, 5)
+  println(a ++ b ++ c ++ d)
 }
